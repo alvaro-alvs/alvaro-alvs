2a13f9d9b7e6b0c24f2db8ae0c8c8396523e2032
Alvaro <alvaro.nrx88@gmail.com>
2024-08-17 14:22:41 -0300

    backend fix

--- src/components/ui/projects/OxxProjects.jsx
+++ src/components/ui/projects/OxxProjects.jsx
@@ -1,27 +1,27 @@
 import { TechStackBadge } from "../TechStackBadge"
 
-type OxxProject = {
-    id: number,
-    title: string,
-    description: string,
-    link: string,
-    logo: string,
-    tech_stack: string[],
-    order: null,
-    bg_public_url: string
-}
+// type OxxProject = {
+//     id: number,
+//     title: string,
+//     description: string,
+//     link: string,
+//     logo: string,
+//     tech_stack: string[],
+//     order: null,
+//     bg_public_url: string
+// }
 
-type OxxProjectProps = {
-    raw_projects: OxxProject[]
-}
+// type OxxProjectProps = {
+//     raw_projects: OxxProject[]
+// }
 
-export default function OxxProjects({ raw_projects }: OxxProjectProps) {
+export default function OxxProjects({ raw_projects }) {
     const projects = raw_projects.reverse()
 
     return (
         <>
             {
-                Array.isArray(projects) && projects.map((project: any) => (
+                Array.isArray(projects) && projects.map((project) => (
                     <div key={project.id} className="p-0 flex even:flex-row-reverse grid-cols-2 rounded-lg borde border-gray-800 min-h-52 overflow-hidden group cursor-pointer shadow-lg">
                         <header className="w-1/2 overflow-hidden flex items-center">
                             <img
@@ -38,7 +38,7 @@ export default function OxxProjects({ raw_projects }: OxxProjectProps) {
                             <p className="project-desc text-sm max-h-20 overflow-y-scroll no-scrollbar">{project.description}</p>
 
                             <footer className="flex flex-wrap gap-3">
-                                {project.tech_stack.map((tech: string) => (
+                                {project.tech_stack.map((tech) => (
                                     <TechStackBadge> {tech} </TechStackBadge>
                                 ))}
                             </footer>
